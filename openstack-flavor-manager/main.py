24a77c25a0c4e0eff04253eb5f3172091133d7f1
Tim Beermann <beermann@osism.tech>
2023-03-16 13:23:31 +0100

    Make the flavor manager functional. (#9)
    You can use the flavor manager to create OpenStack flavors. The flavors might be provided via a URL to a yaml file.
    
    closes #1
    closes #2
    
    Signed-off-by: Tim Beermann <beermann@osism.tech>

--- openstack-flavor-manager/main.py
+++ openstack-flavor-manager/main.py
@@ -2,16 +2,8 @@ import typer
 
 from cloud import Cloud
 from ensure import Ensure
-from validate import Validate
 
 app = typer.Typer(help="Client to manage OpenStack flavors")
-
-
-@app.command("validate")
-def validate(url: str = typer.Argument("scs"), cloudbackend: str = typer.Option("openstack")) -> None:
-    cloud = Cloud(backend=cloudbackend)
-    object = Validate(cloud=cloud, url=url)
-    object.validate()
 
 
 @app.command("ensure")
--- openstack-flavor-manager/reference.py
+++ openstack-flavor-manager/reference.py
@@ -4,7 +4,7 @@ import yaml
 
 def get_url(url: str, key: str) -> list:
     if url == "scs":
-        url = "https://raw.githubusercontent.com/SovereignCloudStack/standards/main/flavors.yaml"
+        url = "https://raw.githubusercontent.com/SovereignCloudStack/standards/main/Tests/iaas/SCS-Spec.MandatoryFlavors.verbose.yaml"
     if url == "osism":
         url = "https://raw.githubusercontent.com/osism/openstack-flavor-manager/main/flavors.yaml"
 
@@ -13,5 +13,15 @@ def get_url(url: str, key: str) -> list:
         result = yaml.safe_load(result.content)
     except yaml.YAMLError as exc:
         print(exc)
+
+    # depending on the source of origin, the data might need to be cleaned up
+    cleaned_list = []
+    for item in result[key]:
+        # scs exports a field description which has to be ignored
+        if "description" in item:
+            item.pop("description")
+        cleaned_list.append(item)
+
+    result[key] = cleaned_list
 
     return result[key]
