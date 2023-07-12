5c243a0e399c2ef8f317c26c9207ef1d617f017d
Gondermann <gondermann@b1-systems.de>
2023-07-12 12:23:00 +0200

    Refactored existing files to get rid of dead code
    Previously, a lot of existing code was never used. Using linting and
    type checking, we have been able to detect that and remove said code.
    This commit should now pass the flake8 linting test and the mypy
    type checking.
    
    Signed-off-by: Gondermann <gondermann@b1-systems.de>

---
+++ openstack_flavor_manager/reference.py
@@ -0,0 +1,19 @@
+import logging
+import requests
+import yaml
+
+
+def get_url(url: str) -> dict:
+    if url == "scs":
+        url = "https://raw.githubusercontent.com/SovereignCloudStack/standards/main/Tests/iaas/SCS-Spec.MandatoryFlavors.verbose.yaml"  # noqa: E501
+    if url == "osism":
+        url = "https://raw.githubusercontent.com/osism/openstack-flavor-manager/main/flavors.yaml"
+
+    logging.debug(f"Loading flavor definitions from {url}")
+
+    result = requests.get(url)
+
+    try:
+        return yaml.safe_load(result.content)
+    except yaml.YAMLError as exc:
+        logging.error(exc)
