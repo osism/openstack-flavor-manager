c938104fa661a59473084d44fef057bcdf9159af
lindenb1 <138122960+lindenb1@users.noreply.github.com>
2023-07-20 11:16:26 +0200

    Adding --recommended typer option (#45)
    adding the possibilty to run the script with --recommended option
    to install the recommended flavors
    
    fixes #44
    Signed-off-by: Robin van der Linden <linden@b1-systems.de>

--- openstack_flavor_manager/main.py
+++ openstack_flavor_manager/main.py
@@ -8,7 +8,8 @@ app = typer.Typer(help="Client to manage OpenStack flavors")
 
 
 @app.command("ensure")
-def ensure(url: str, cloud_backend: str = typer.Option("openstack"), recommended: bool = False) -> None:
+def ensure(url: str, cloud_backend: str = typer.Option("openstack"),
+           recommended: bool = typer.Option(False, help="Use recommended settings")) -> None:
     flavour_definitions = get_url(url)
     cloud = Cloud()
     ensure_object = Ensure(cloud=cloud, definitions=flavour_definitions, recommended=recommended)
