68de7c8837d4ed49fedd3afc1ec679e9b3ebbe7a
Matthias Büchse <matthias.buechse@cloudandheat.com>
2023-11-17 21:11:33 +0100

    extra_specs are now set according to flavor_spec
    Signed-off-by: Matthias Büchse <matthias.buechse@cloudandheat.com>

--- openstack_flavor_manager/main.py
+++ openstack_flavor_manager/main.py
@@ -59,9 +59,17 @@ class Cloud:
                 key_string="public", flavor_spec=flavor_spec, defaults=defaults
             ),
         )
+        extra_specs = {
+            key: value
+            for key, value in flavor_spec.items()
+            # we could exclude keys explicitly, like so:
+            # if key not in ('name', 'ram', 'cpus', 'disk', 'public', 'disabled')
+            # but the extra specs should be prefixed, so we can as well do it like so:
+            if ':' in key
+        }
         self.conn.set_flavor_specs(
             flavor_id=flavor.id,
-            extra_specs={},
+            extra_specs=extra_specs,
         )
         return flavor
 
